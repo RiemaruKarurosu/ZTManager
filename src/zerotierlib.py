823d2578da502c3830679163bc77ce586d9f993c
Riemaru Karurosu <45057400+RiemaruKarurosu@users.noreply.github.com>
2025-02-20 14:29:10 -0600

    Update zerotierlib.py

--- src/zerotierlib.py
+++ src/zerotierlib.py
@@ -8,6 +8,7 @@ from pathlib import Path
 import requests
 import subprocess
 import os
+import json
 
 
 class ZeroTierNetwork:
@@ -16,70 +17,45 @@ class ZeroTierNetwork:
     PATH = Path.home() / '.config' / 'ztlib'
     FILE = 'zt.conf'
     SERVICE = 'zerotier-one.service'
+    URL = 'https://localhost:9993/'
 
-    def __init__(self, api_token=None):
+    def __init__(self, api_token: Optional[str] = None):
         self.api_token = api_token
         self.serviceStatus = None
         self.listNetworks = []
         if self.api_token:
             self.headers = {'X-ZT1-Auth': f'{api_token}'}
-            print(self.api_token)
         else:
             self.headers = None
         print(self.zt_start())
 
-    # Maybe Work(?
     def zt_start(self) -> str:
         try:
-            if not self.check_token(self.api_token):
-                if self.read_token() == 401 or self.read_token() == 404:
-                    if self.check_token(self.get_token()):
-                        return 'OK'
-                    else:
-                        return 'MISSING ROOT PERMISSION'
+            if self.check_token(self.api_token):
                 return 'OK'
+            
+            if self.read_token() in (401, 404):
+                if self.check_token(self.get_token()):
+                    return 'OK'
+                return 'MISSING ROOT PERMISSION'
+            
             return 'OK'
-        except Exception:
-            return 'MISSING ROOT PERMISSIONS EXCEPTION'
+        except Exception as e:
+            return f'MISSING ROOT PERMISSIONS EXCEPTION: {e}'
 
-
-
-    # ZeroTier Status:
-
-    # Check if ZeroTier-One is active
-    # New Pydbus implementation
     def zt_status(self) -> bool:
-        try:
-            bus = SystemBus()
-            systemd = bus.get(".systemd1")
-            for unit in systemd.ListUnits():
-                if unit[0] == self.SERVICE:
-                    if unit[3] == "active" and unit[4] == 'running':
-                        return True
-                    else:
-                        return False
-            return False
-        except Exception:
-            return False
-
+        unit = self.get_systemd_unit(self.SERVICE)
+        if unit and unit[3] == "active" and unit[4] == 'running':
+            return True
+        return False
 
 
     def zt_enable_status(self) -> bool:
-        try:
-            bus = SystemBus()
-            systemd = bus.get(".systemd1")
-            state = systemd.GetUnitFileState(self.SERVICE)
-            print(state)
-            if state == 'enabled':
-                return True
-            elif state == 'disabled':
-                return False
-        except Exception:
-            return False
-
-
-
-    # Status ZeroTier-One service
+        unit = self.get_systemd_unit(self.SERVICE)
+        if unit:
+            return unit[3] == 'enabled'
+        return False
+        
     def service(self, setstatus):
         if setstatus:
             try:
@@ -87,81 +63,68 @@ class ZeroTierNetwork:
                 self._zt_activate()
                 return True
             except Exception as e:
-                print(e)
+                print(f"Error al cambiar el estado del servicio: {e}")
                 return False
-
+                
     def _zt_activate(self):
         bus = SystemBus()
         systemd = bus.get(".systemd1")
-        if self.serviceStatus == self.COMMANDS[0]:
-            response = systemd.StartUnit(self.SERVICE, "replace")
-            print(response)
-        elif self.serviceStatus == self.COMMANDS[1]:
-            systemd.StopUnit(self.SERVICE, "replace")
-        elif self.serviceStatus == self.COMMANDS[2]:
-            systemd.EnableUnitFiles(['zerotier-one.service'], False, True)
-            systemd.Reload()
-        elif self.serviceStatus == self.COMMANDS[3]:
-            systemd.DisableUnitFiles(['zerotier-one.service'], False)
-            systemd.Reload()
-        else:
-            print("Error, no existe")
-
+        
+        actions = {
+            self.COMMANDS[0]: lambda: systemd.StartUnit(self.SERVICE, "replace"),
+            self.COMMANDS[1]: lambda: systemd.StopUnit(self.SERVICE, "replace"),
+            self.COMMANDS[2]: lambda: (systemd.EnableUnitFiles([self.SERVICE], False, True), systemd.Reload()),
+            self.COMMANDS[3]: lambda: (systemd.DisableUnitFiles([self.SERVICE], False), systemd.Reload())
+        }
+        
+        action = actions.get(self.serviceStatus)
+        
+        if action:
+            action()
+        
         self.serviceStatus = None
 
-    # Save apiToken to ~/.config
-    def read_token(self) -> int:
-        configpath = self.PATH / self.FILE
-        if configpath.exists():
-            with open(configpath, 'r') as configfile:
-                apitoken = configfile.readlines()
-            for token in apitoken:
-                if token.startswith('X-ZT1-Auth'):
-                    key, value = token.split('=')
-                    api_token = value.strip()
-                    if self.check_token(api_token):
-                        self.api_token = api_token
-                        self.headers = {'X-ZT1-Auth': f'{api_token}'}
-                    else:
-                        return 401
-            return 200
-        else:
-            return 404
-
-    def check_token(self, api_token):
-        url = self.URL + 'status'
-        header = {'X-ZT1-Auth': f'{api_token}'}
-        try:
-            response = requests.get(url, headers=header)
-        except Exception:
-            return False
-        if response.status_code == 200:
-            return True
-        elif response.status_code == 401:
-            return False
-        else:
-            return False
 
     def save_token(self):
+        config = {'X-ZT1-Auth': self.api_token}
+        configpath = self.PATH / self.FILE
         if not self.PATH.exists():
             self.PATH.mkdir(parents=True)
-        configpath = self.PATH / self.FILE
         if not configpath.exists():
             configpath.touch()
 
         with open(configpath, 'w') as configfile:
-            configfile.write(f'X-ZT1-Auth = {self.api_token} \n')
-
+            json.dump(config, configfile, indent=4)
         os.chmod(configpath, 0o600)
-        print(configpath)
+        print(f"Token guardado en: {configpath}")
 
-    # Get the Token to use ZeroTierOne Service
-    def get_token(self):
+    def read_token(self) -> int:
+        configpath = self.PATH / self.FILE
+        if not configpath.exists():
+            return 404
+        with open(configpath, 'r') as configfile:
+            config = json.load(configfile)
+        api_token = config.get('X-ZT1-Auth')
+        if api_token and self.check_token(api_token):
+            self.api_token = api_token
+            self.headers = {'X-ZT1-Auth': f'{api_token}'}
+            return 200
+        return 401
+        
+
+    def check_token(self, api_token: str) -> bool:
+        try:
+            url = self.URL + 'status'
+            response = requests.get(url, headers={'X-ZT1-Auth': api_token})
+            return response.status_code == 200
+        except requests.exceptions.RequestException:
+            return False
+
+    def get_token(self) -> Optional[str]:
         try:
             cmd = "flatpak-spawn --host pkexec cat /var/lib/zerotier-one/authtoken.secret"
             result = subprocess.run(cmd, shell=True, capture_output=True, text=True, check=True)
             clave_api = result.stdout.strip()
-            print(f'Saving API: {clave_api}')
             if self.check_token(clave_api):
                 self.api_token = clave_api
                 self.headers = {'X-ZT1-Auth': f'{self.api_token}'}
@@ -169,40 +132,31 @@ class ZeroTierNetwork:
                 return clave_api
         except subprocess.CalledProcessError:
             return 'Error[01]: MissingRootAdmin'
-
-        # ListNetworks
-
-    def get_networks(self, network=None):
-        if network:
-            url = self.URL + 'network/' + network
-        else:
-            url = self.URL + 'network'
-        response = requests.get(url, headers=self.headers)
-        return response.json()
-
-    def join_networks(self, network):
-        url = self.URL + 'network/' + network
-        print(url)
-        response = requests.post(url, headers=self.headers)
-        return response.json()
-
-    def update_network(self, network, config):
-        # Version 2.0 plan.
+            
+    def send_request(self, method: str, endpoint: str, data: Optional[dict] = None):
+        try:
+            url = self.BASE_URL + endpoint
+            response = getattr(requests, method)(url, headers=self.headers, json=data)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Error en la solicitud: {e}")
+            return None
+    
+    def get_networks(self, network: Optional[str] = None):
+        endpoint = f'network/{network}' if network else 'network'
+        return self.send_request('get', endpoint)
+        
+    def join_networks(self, network: str):
+        return self.send_request('post', f'network/{network}')
+        
+    def update_network(self, network: str, config: dict):
+        # Plan para la versión 2.0
         pass
 
-    def leave_networks(self, network):
-        url = self.URL + 'network' + network
-        response = requests.delete(url, headers=self.headers)
-        print(response)
-        return response
-        # Peers
+    def leave_networks(self, network: str):
+        return self.send_request('delete', f'network/{network}')
 
-    def get_peers(self, network=None):
-        if network:
-            url = self.URL + 'peer/' + network
-        else:
-            url = self.URL + 'peer'
-        response = requests.get(url, headers=self.headers)
-        print(response.json())
-        return response.json()
-
+    def get_peers(self, network: Optional[str] = None):
+        endpoint = f'peer/{network}' if network else 'peer'
+        return self.send_request('get', endpoint)
